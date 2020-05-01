a6a3d2cfe21150a9e49ff8ae1d930a09996af21e
Isaac Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-05-01 13:55:41 -0700

    add additional functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-jQuery(document).ready(function() {
+function chop(sentence) {
+   var result = (sentence.charAt(0) + sentence.charAt(sentence.length-1));
+   consoleResult = result.toUpperCase();
+   console.log(consoleResult);
+};
+
+function reverse(consoleResult) {
+    consoleResult2 = consoleResult.split("").reverse().join("");
+    console.log(consoleResult2);
+}
+
+function conversion(sentence) {
+    chop(sentence);
+    reverse(consoleResult);
+    return sentence.concat(consoleResult2);
+}
+
+$(document).ready(function() {
     var sentence = prompt("enter a sentence to be ciphered");
-    
+    console.log(sentence);
+    conversion(sentence);
+    console.log(conversion(sentence));
   });
