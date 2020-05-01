2e86b34773f1f73d3f47eca79a4af2dd9ae9b371
Isaac Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-05-01 14:30:02 -0700

    ensure all functionality works

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,39 @@
 function chop(sentence) {
    var result = (sentence.charAt(0) + sentence.charAt(sentence.length-1));
-   consoleResult = result.toUpperCase();
-   console.log(consoleResult);
+   return result.toUpperCase();
 };
 
 function reverse(consoleResult) {
-    consoleResult2 = consoleResult.split("").reverse().join("");
-    console.log(consoleResult2);
+   return consoleResult.split("").reverse().join("");
+}
+
+function attach(consoleResult2, sentence) {
+   return sentence.concat(consoleResult2);
+}
+
+function bounce(consoleResult3) {
+   var letter = (consoleResult3.charAt(consoleResult3.length / 2));
+   console.log(letter);
+   return letter.concat(consoleResult3);
+}
+
+function superReverse(consoleResult4) {
+    var consoleResult5 = consoleResult4.split("").reverse().join("");
+    console.log(consoleResult5);
 }
 
 function conversion(sentence) {
-    chop(sentence);
-    reverse(consoleResult);
-    return sentence.concat(consoleResult2);
+    var consoleResult = chop(sentence);
+    console.log(consoleResult);
+    var consoleResult2 = reverse(consoleResult);
+    console.log(consoleResult2);
+    var consoleResult3 = attach(consoleResult2, sentence);
+    console.log(consoleResult3);
+    var consoleResult4 = bounce(consoleResult3);
+    superReverse(consoleResult4);
 }
 
 $(document).ready(function() {
     var sentence = prompt("enter a sentence to be ciphered");
-    console.log(sentence);
     conversion(sentence);
-    console.log(conversion(sentence));
   });
